872c435fc0e861290dae399d7bd99729ce63e1a4
Ethan Qu <quqi0714@gmail.com>
2026-04-26 17:12:55 -0700

    Initial Commit

---
+++ CleanFast/CleanFastApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanFastApp.swift
+//  CleanFast
+//
+//  Created by Qu on 4/26/26.
+//
+
+import SwiftUI
+
+@main
+struct CleanFastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CleanFastTests/CleanFastTests.swift
@@ -0,0 +1,19 @@
+//
+//  CleanFastTests.swift
+//  CleanFastTests
+//
+//  Created by Qu on 4/26/26.
+//
+
+import Testing
+@testable import CleanFast
+
+struct CleanFastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
